e941101a48d51644248dd81eb9e3218c9b9903ad
Will Dollman <will@dollman.org>
2012-09-13 18:16:01 +0100

    Parse coords and send multiple waypoints in 1 file (not via MotionX)
    Turns out the MotionX app can read GPX files, and you can specify
    multiple waypoints per file. So instead of going round the
    'email to motionx' loop, just email directly to me and open the file

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
 // should be changed to POST later
-$geocache_json = $_GET['geocache_json'];
+$geocache_json = $_REQUEST['geocache_json'];
 
 //var_dump( json_decode($geocache_json) );
 $geocache = json_decode($geocache_json);
@@ -13,63 +13,56 @@ $geocache = json_decode($geocache_json);
 // this should be done in geocachetoXML
 
 //loop through geocache[ ]'s
-$geoXML[0] = geocachetoXML($geocache[0]);
-$geoXML[1] = geocachetoXML($geocache[0]);
+$geoXML[0] = geocachetoXML($geocache);
+
+print "XML is:<br><br>$geoXML[0]";
+
+print "Number of waypoints: ". multi_attach_mail("will@dollman.org", $geoXML, "will@dollman.org", count($geocache));
 
 
-//print "Result is ". multi_attach_mail("will@dollman.org", $geoXML, "will@dollman.org");
-print "Number of waypoints: ". multi_attach_mail("will@dollman.org", $geoXML, "will@dollman.org");
-
-
-function multi_attach_mail($to, $geodata, $sendermail) 
-{
-    // email fields: to, from, subject, and so on
-    //$from = "Files attach <".$sendermail.">"; 
-    $from = "will@dollman.org";
-    $subject = date("d.M H:i")." F=".count($geodata); 
-    $message = date("Y.m.d H:i:s")."\n".count($geodata)." attachments";
-    $headers = "From: will@dollman.org";
- 
-    // boundary 
-    $semi_rand = md5(time()); 
-    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
-
-    // headers for attachment 
-    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
- 
-    // multipart boundary 
-    $message = "--{$mime_boundary}\n" . "Content-Type: text/plain; charset=\"iso-8859-1\"\n" .
-    "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n"; 
-    // preparing attachments
-    for($i=0;$i<count($geodata);$i++){
-            $message .= "--{$mime_boundary}\n";
-            $chunkdata = chunk_split(base64_encode($geodata[$i]));
-            $message .= "Content-Type: application/octet-stream; name=\"data.gpx\"\n";
-            $message .= "Content-Description: data.gpx\n";
-            $message .= "Content-Disposition: attachment;\n" . " filename=\"gpsdata$i.gpx\"; size=100;\n";
-            $message .= "Content-Transfer-Encoding: base64\n\n" . $chunkdata . "\n\n";
-        }
-    $message .= "--{$mime_boundary}--";
-    $returnpath = "-f" . $sendermail;
-    $ok = @mail($to, $subject, $message, $headers); 
-    if($ok){ return $i; } else { return 0; }
-}
 
 
 // Make sure that all variables are encoded to REMOVE XML characters!
 function geocachetoXML($geocache)
 {
-    $xml = <<<EOD
+
+// gpx header
+$xml = <<<EOD
 <gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" creator="MotionXGPSFull 19.1 Build 4444R">
-<wpt lat="51.3638166667" lon="-2.7853500000">
-<name>{$geocache->{title}}</name>
-<desc>{$geocache->{shortDesc}} ||| {$geocache->{longDesc}}</desc>
-</wpt>
+
+EOD;
+
+    // insert waypoints
+    for($i=0; $i<count($geocache); $i++) {
+        $xml .= createWaypoint($geocache[$i]);
+    }
+
+// gpx footer
+$xml .= <<<EOD
 </gpx>
 EOD;
 
     return $xml;
+}
 
+
+function createWaypoint($geodata)
+{
+
+preg_match('/N\s(\d*)\s([\d.]*)\sW\s(\d*)\s([\d.]*)/', $geodata->{location}, $coordPart);
+
+$geodata->{latitude} = DMStoDEC( $coordPart[1], $coordPart[2], 0 );
+$geodata->{longitude} = -1 * DMStoDEC( $coordPart[3], $coordPart[4], 0 );
+
+$waypointData = <<<EOD
+<wpt lat="{$geodata->{latitude}}" lon="{$geodata->{longitude}}">
+<name>{$geodata->{title}}</name>
+<desc>{$geodata->{shortDesc}} ||| {$geodata->{longDesc}}</desc>
+</wpt>
+
+EOD;
+
+    return $waypointData;
 }
 
 function DMStoDEC($deg,$min,$sec)
@@ -102,5 +95,41 @@ function DECtoDMS($dec)
 
     return array("deg"=>$deg,"min"=>$min,"sec"=>$sec);
 }   
+
+
+
+function multi_attach_mail($to, $geodata, $sendermail, $geoCount) 
+{
+    // email fields: to, from, subject, and so on
+    //$from = "Files attach <".$sendermail.">"; 
+    $from = "will@dollman.org";
+    $subject = date("d.M H:i")." F=".count($geodata); 
+    $message = "GPS import from Geocaching-MotionX importer\nContains $geoCount datapoints";
+    $headers = "From: will@dollman.org";
+ 
+    // boundary 
+    $semi_rand = md5(time()); 
+    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+
+    // headers for attachment 
+    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+ 
+    // multipart boundary 
+    $message = "--{$mime_boundary}\n" . "Content-Type: text/plain; charset=\"iso-8859-1\"\n" .
+    "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n"; 
+    // preparing attachments
+    for($i=0;$i<count($geodata);$i++){
+            $message .= "--{$mime_boundary}\n";
+            $chunkdata = chunk_split(base64_encode($geodata[$i]));
+            $message .= "Content-Type: application/octet-stream; name=\"data.gpx\"\n";
+            $message .= "Content-Description: data.gpx\n";
+            $message .= "Content-Disposition: attachment;\n" . " filename=\"gpsdata$i.gpx\"; size=100;\n";
+            $message .= "Content-Transfer-Encoding: base64\n\n" . $chunkdata . "\n\n";
+        }
+    $message .= "--{$mime_boundary}--";
+    $returnpath = "-f" . $sendermail;
+    $ok = @mail($to, $subject, $message, $headers); 
+    if($ok){ return $i; } else { return 0; }
+}
 
 ?>
